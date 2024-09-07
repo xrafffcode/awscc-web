1424cd02b6759e250b2199f0ad59f5d4f4f69779
xrafffcode <yusfiwawan2@gmail.com>
2024-09-07 19:43:25 +0700

    update

--- src/components/HomeAbout.vue
+++ src/components/HomeAbout.vue
@@ -1,41 +1,33 @@
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue'
+import { ref } from 'vue'
 
-const paragraphs = ref([
-    `AWS Cloud Community (AWSCC) is a global community dedicated to AWS users. We focus on learning, developing, and collaborating through a variety of events, training, and in-depth discussions.`,
-    `AWSCC has grown into a dynamic and inclusive community since its inception, with members from diverse backgrounds and skill levels. We believe the power of cloud computing lies in collaboration and shared learning.`,
-    `Whether you're a beginner or an expert in AWS, AWSCC provides a space for you to learn and share knowledge in a supportive and collaborative environment.`
-])
+const description = ref(`
+    <p class="about-us-description">
+            AWS Cloud Community (AWSCC) is a global community dedicated to AWS users. We focus on
+            learning,
+            developing,
+            and collaborating through a variety of events, training, and in-depth discussions.
+    
+    </p>
 
-const paragraphStyles = ref([])
-const observer = ref(null)
+    <p class="about-us-description">
+        AWSCC has grown into a dynamic and inclusive community since its inception, with members from
+        diverse
+        backgrounds and skill levels. We believe the power of cloud computing lies in collaboration and
+        shared
+        learning.
+    </p>
 
-onMounted(() => {
-    observer.value = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting) {
-            paragraphs.value.forEach((_, index) => {
-                const animationDelay = `${index * 1.5}s`
-                paragraphStyles.value.push({
-                    animationDelay: animationDelay,
-                    animationDuration: '1.5s',
-                    animationFillMode: 'forwards',
-                    animationTimingFunction: 'ease-in-out'
-                })
-            })
-        } else {
-            paragraphStyles.value = []
-        }
-    }, { root: null, rootMargin: '0px', threshold: 1.0 })
-
-    observer.value.observe(document.getElementById('about'))
-})
-
-onUnmounted(() => {
-    observer.value.disconnect()
-})
+    <p class="about-us-description">
+        Whether you're a beginner or an expert in AWS, AWSCC provides a space for you to learn and share
+        knowledge
+        in a supportive and collaborative environment.
+    </p>
+`)
 </script>
 
 <template>
+
     <section class="about-us py-5" id="about">
         <div class="container">
             <div class="row">
@@ -45,31 +37,8 @@ onUnmounted(() => {
                     <img src="@/assets/images/logo.png" alt="about-us" class="img-fluid about-us-logo">
                 </div>
 
-                <div class="col-12 col-md-6 mt-5 mt-md-0 mt-lg-0">
-                    <div v-for="(paragraph, index) in paragraphs" :key="index">
-                        <p class="about-us-description" :style="paragraphStyles[index]">
-                            {{ paragraph }}
-                        </p>
-                    </div>
-                </div>
+                <div class="col-12 col-md-6 mt-5 mt-md-0 mt-lg-0" v-html="description"></div>
             </div>
         </div>
     </section>
 </template>
-
-<style>
-@keyframes color-change {
-    0% {
-        color: rgba(124, 122, 133, 1);
-    }
-
-    100% {
-        color: black;
-    }
-}
-
-.about-us-description {
-    color: rgba(124, 122, 133, 1);
-    animation-name: color-change;
-}
-</style>
